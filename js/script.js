d5517b87a5f6e8a498b86a63e38ff5153fbbd894
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-21 14:49:14 +0000

    ticket

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // ELEMENTS
 
 const priceKm = 0.21 ;
+const fullname = document.querySelector('.container .inputfullname');
 
 
 
 // INPUT
-const inputfullname = document.querySelector('fullname');
-const inputkm = document.querySelector('km');
+const inputfullname = document.querySelector('inputfullname');
+const inputkm = document.querySelector('inputkm');
 
 console.log(inputfullname);
 // BUTTONS
