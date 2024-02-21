b2bd986dc8dd5e0e4aeaeb6153bd01d4bdb87a78
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-21 15:19:19 +0000

    post ticket

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
 // ELEMENTS
 
 const priceKm = 0.21 ;
-const fullname = document.querySelector('.container .inputfullname');
-
+const ticket = document.querySelector('.ticket');
 
 
 // INPUT
-const inputfullname = document.querySelector('inputfullname');
-const inputkm = document.querySelector('inputkm');
+const inputfullname = document.querySelector('.inputfullname');
+const inputkm = document.querySelector('.inputkm');
 
-console.log(inputfullname);
 // BUTTONS
+const generate = document.querySelector('.submit');
+const reset = document.querySelector('.reset');
+
+
+genera.addEventListener('click', function(){
+  // ticket.classList.toggle('d-block'); NON FUNZIONA
+  const name = inputfullname.value;
+  document.querySelector('.fullname').innerHTML += name;
+  // inserire offerta in base al select
+  // generare numero random carrozza
+  // generare numero random booking
+  // calcolo costo biglietto
+})
