37d194f8fe172654aed1c8605e7ba38fcae50cf2
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-21 15:45:07 +0000

    numeri random

--- js/script.js
+++ js/script.js
@@ -9,16 +9,21 @@ const inputfullname = document.querySelector('.inputfullname');
 const inputkm = document.querySelector('.inputkm');
 
 // BUTTONS
-const generate = document.querySelector('.submit');
+const generate = document.querySelector('.generate');
 const reset = document.querySelector('.reset');
 
 
-genera.addEventListener('click', function(){
+generate.addEventListener('click', function(){
   // ticket.classList.toggle('d-block'); NON FUNZIONA
   const name = inputfullname.value;
-  document.querySelector('.fullname').innerHTML += name;
+  document.querySelector('.ticket .fullname').innerHTML += name;
   // inserire offerta in base al select
+  
   // generare numero random carrozza
+  const wagonrandom = Math.floor(Math.random() * 11);
+  document.querySelector('.ticket .wagon').innerHTML += wagonrandom;
   // generare numero random booking
+  const bookingNumber = Math.floor(Math.random() * 90000) + 10000;
+  document.querySelector('.ticket .bookingID').innerHTML += bookingNumber;
   // calcolo costo biglietto
 })
